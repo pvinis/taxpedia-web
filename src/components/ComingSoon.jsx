58b47d4f32e2e1a6b857c26c983d7629814d91af
Pavlos Vinieratos <pvinis@gmail.com>
2020-02-27 15:52:46 +0400

    some

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 
-export default () => <div style={{ margin: '10%', textAlign: 'center'}}>
-    <h1>Coming Soon</h1>
-    <p>A wonderful new website!</p>
-    <p>Please check back again soon.</p>
+export default () => <div>
+	<div style={{width: '100%', position: 'absolute', height: '100%', backgroundImage: 'url(bg.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} />
+	<div style={{display: 'flex',position: 'absolute', width: '50%', height:'100%', color: 'white',  alignItems: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+    <h1 style={{fontSize: 70}}>Taxpedia</h1>
+    <p style={{fontSize: 30}}>Please check back again soon.</p>
+	</div>
 </div>
